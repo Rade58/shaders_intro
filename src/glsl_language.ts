6633c5670da581cc693e0777a38bc855af8ee609
rade <bajic.rade2@gmail.com>
2024-11-02 18:43:37 +0100

    glsl functions and native functions

--- src/glsl_language.ts
+++ src/glsl_language.ts
@@ -77,7 +77,7 @@ import fragmentShader from "./shaders/test/fragment.glsl";
 
 // Some of them are    sin, cos, max, min, pow, exp, mod, clamp
 
-// also, a lot of practical ones like:
+// also, there are a lot of practical, but complex ones, like:
 //         cross, distance, reflect, refract, normalize
 
 // -------------------------------------------------------------
