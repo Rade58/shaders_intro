de6d44a7a738d32665272b059416a5e503d6dabb
rade <bajic.rade2@gmail.com>
2024-11-03 23:20:43 +0100

    🚀

---
+++ src/shaders/uniforms/fragment.glsl
@@ -0,0 +1,18 @@
+precision mediump float;
+
+// varying float vRandom;
+
+
+void main() {
+
+
+  // vec3 foo = vRandom * vec3(0.1, 0.5, 0.1);
+  // gl_FragColor = vec4(foo, 1.0);
+
+  // gl_FragColor = vec4(vRandom, 0.4, 0.4, 1.0);
+
+  gl_FragColor = vec4(0.6, 0.5, 0.3, 1.0);
+
+
+
+}
